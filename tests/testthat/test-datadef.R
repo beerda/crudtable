f56b314280a40bd96d7608b43f6ead2b2a3f0c8c
Michal Burda <michal.burda@centrum.cz>
2020-04-10 16:41:06 +0200

    Fixed datadef test

--- tests/testthat/test-datadef.R
+++ tests/testthat/test-datadef.R
@@ -4,14 +4,14 @@ test_that("datadef default values loading", {
     expect_true(is.datadef(res))
     expect_equal(res,
                  list(table='tab',
-                      columns=list(a=list(name='a', type='text', na=FALSE, persistent=TRUE),
-                                   b=list(name='b', type='text', na=FALSE, persistent=TRUE))))
+                      columns=list(a=list(name='a', type='text', na=FALSE, persistent=TRUE, default=''),
+                                   b=list(name='b', type='text', na=FALSE, persistent=TRUE, default=''))))
 
     res <- datadef(list(table='tab',
                               columns=list(a=list(name='Bla'), b=list(na=TRUE))))
     expect_true(is.datadef(res))
     expect_equal(res,
                  list(table='tab',
-                      columns=list(a=list(name='Bla', type='text', na=FALSE, persistent=TRUE),
-                                   b=list(na=TRUE, name='b', type='text', persistent=TRUE))))
+                      columns=list(a=list(name='Bla', type='text', na=FALSE, persistent=TRUE, default=''),
+                                   b=list(na=TRUE, name='b', type='text', persistent=TRUE, default=''))))
 })
