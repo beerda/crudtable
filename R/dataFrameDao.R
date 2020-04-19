3c4e6cc6cbf57ef9039af33d92b978669c0143a9
Michal Burda <michal.burda@centrum.cz>
2020-04-19 11:01:43 +0200

    Added dataFrameDao()

---
+++ R/dataFrameDao.R
@@ -0,0 +1,47 @@
+#' @export
+dataFrameDao <- function(d) {
+    assert_that(is.data.frame(d))
+    assert_that(all(colnames(d) != 'id'))
+
+    attributes <- colnames(d)
+    data <- cbind(id=seq_len(nrow(d)), d)
+
+    structure(list(
+        getAttributes = function() {
+            attributes
+        },
+
+        getData = function() {
+            data
+        },
+
+        getRecord = function(id) {
+            assert_that(is.scalar(id) && is.numeric(id))
+            data[data$id == id, ]
+        },
+
+        insert = function(record) {
+            assert_that(is.list(record))
+            assert_that(length(setdiff(attributes, names(record))) == 0)
+            record$id <- max(data$id) + 1
+            data <<- rbind(data, record[colnames(data)])
+            invisible(1)
+        },
+
+        update = function(id, record) {
+            assert_that(is.scalar(id) && is.numeric(id))
+            assert_that(is.list(record))
+            assert_that(length(setdiff(attributes, names(record))) == 0)
+            data[data$id == id, attributes] <<- record[attributes]
+            rownames(data) <<- NULL
+            invisible(1)
+        },
+
+        delete = function(id) {
+            assert_that(is.scalar(id) && is.numeric(id))
+            data <<- data[data$id != id, ]
+            rownames(data) <<- NULL
+            invisible(1)
+        }
+    ), class = 'dao')
+}
---
+++ tests/testthat/test-dataFrameDao.R
@@ -0,0 +1,79 @@
+test_that("dataFrameDao::getAttributes", {
+    d <- CO2[1:5, ]
+    dao <- dataFrameDao(d)
+
+    expect_equal(dao$getAttributes(), c('Plant', 'Type', 'Treatment', 'conc', 'uptake'))
+})
+
+
+test_that("dataFrameDao::getData", {
+    d <- CO2[1:5, ]
+    dao <- dataFrameDao(d)
+
+    expect_equal(dao$getData(), cbind(id=1:5, CO2[1:5, ]))
+
+    d <- CO2[1:3, ]
+    expect_equal(dao$getData(), cbind(id=1:5, CO2[1:5, ]))
+
+    d <<- CO2[1:3, ]
+    expect_equal(dao$getData(), cbind(id=1:5, CO2[1:5, ]))
+
+    res <- dao$getData()
+    res[1, 1] <- NA
+    expect_equal(dao$getData(), cbind(id=1:5, CO2[1:5, ]))
+})
+
+
+test_that("dataFrameDao::getRecord", {
+    d <- CO2[1:5, ]
+    dao <- dataFrameDao(d)
+
+    expect_equal(dao$getRecord(2), cbind(id=2, CO2[2, ]))
+})
+
+
+test_that("dataFrameDao::insert", {
+    d <- CO2[1:5, ]
+    dao <- dataFrameDao(d)
+    dao$insert(as.list(CO2[6, ]))
+    expect_equal(dao$getData(), cbind(id=1:6, CO2[1:6, ]))
+})
+
+
+test_that("dataFrameDao::update", {
+    d <- CO2[1:5, ]
+    dao <- dataFrameDao(d)
+    dao$update(3, as.list(CO2[6, ]))
+    exp <- cbind(id=1:5, CO2[c(1,2,6,4,5), ])
+    rownames(exp) <- NULL
+    expect_equal(dao$getData(), exp)
+})
+
+
+test_that("dataFrameDao::update non continuous ids", {
+    d <- CO2[1:5, ]
+    dao <- dataFrameDao(d)
+    dao$delete(2)
+    dao$update(3, as.list(CO2[6, ]))
+    exp <- cbind(id=c(1,3:5), CO2[c(1,6,4,5), ])
+    rownames(exp) <- NULL
+    expect_equal(dao$getData(), exp)
+})
+
+
+test_that("dataFrameDao::delete", {
+    d <- CO2[1:5, ]
+    dao <- dataFrameDao(d)
+
+    dao$delete(3)
+    exp <- cbind(id=1:5, CO2[1:5, ])
+    exp <- exp[-3, ]
+    rownames(exp) <- NULL
+    expect_equal(dao$getData(), exp)
+
+    dao$delete(5)
+    exp <- cbind(id=1:5, CO2[1:5, ])
+    exp <- exp[c(-3, -5), ]
+    rownames(exp) <- NULL
+    expect_equal(dao$getData(), exp)
+})
