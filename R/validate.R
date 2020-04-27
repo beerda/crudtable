a732d1351cfe46c9845cd557dbfab90b27778a3e
Michal Burda <michal.burda@centrum.cz>
2020-04-27 11:39:16 +0200

    Vectorized validators

--- R/validate.R
+++ R/validate.R
@@ -1,15 +1,22 @@
 #' @export
-validate <- function(inputId, errorMessage, f) {
-    res <- list(list(errorMessage = errorMessage,
-                     f = f))
-    names(res) <- inputId
+validate <- function(inputIds, errorMessages, f) {
+    assert_that(is.character(inputIds))
+    assert_that(is.character(errorMessages))
+    assert_that(length(inputIds) >= length(errorMessages))
+    assert_that(is.function(f))
+
+    errorMessages <- rep_len(errorMessages, length(inputIds))
+    res <- map(errorMessages, function(m) {
+        list(errorMessage = m, f = f)
+    })
+    names(res) <- inputIds
     res
 }
 
 #' @export
-validateNA <- function(inputId, errorMessage = 'Must not be empty.') {
-    validate(inputId,
-             errorMessage,
+validateNA <- function(inputIds, errorMessages = 'Must not be empty.') {
+    validate(inputIds,
+             errorMessages,
              function(v) {
                  !is.null(v) && !is.na(v) && trimws(v) != ''
              })
