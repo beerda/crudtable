d509917a2954f29ba4020890a899e0f663fd9267
Michal Burda <michal.burda@centrum.cz>
2020-04-27 11:48:05 +0200

    Added tests of validate*()

---
+++ tests/testthat/test-validate.R
@@ -0,0 +1,28 @@
+test_that("validate", {
+    v <- validate(letters[1:3], LETTERS[1:3], is.na)
+    expect_true(is.list(v))
+    expect_equal(length(v), 3)
+    expect_equal(names(v), letters[1:3])
+    expect_equal(v[[1]]$errorMessage, 'A')
+    expect_equal(v[[2]]$errorMessage, 'B')
+    expect_equal(v[[3]]$errorMessage, 'C')
+    expect_equal(v[[1]]$f, is.na)
+    expect_equal(v[[2]]$f, is.na)
+    expect_equal(v[[3]]$f, is.na)
+})
+
+
+test_that("validateNA", {
+    v <- validateNA(letters[1:3])
+    expect_true(is.list(v))
+    expect_equal(length(v), 3)
+    expect_equal(names(v), letters[1:3])
+
+    expect_true(v[[1]]$f('xyz'))
+    expect_true(v[[1]]$f(123))
+    expect_true(v[[1]]$f(FALSE))
+    expect_true(v[[1]]$f(1:3))
+    expect_false(v[[1]]$f(NULL))
+    expect_false(v[[1]]$f(NA))
+    expect_false(v[[1]]$f(''))
+})
