aa17f2f57066c473920d6343e8a7142001f38e2f
Michal Burda <michal.burda@centrum.cz>
2020-04-29 11:26:59 +0200

    Implemented simpleFormUIFactory

--- R/simpleFormUIFactory.R
+++ R/simpleFormUIFactory.R
@@ -5,8 +5,17 @@ simpleFormUIFactory <- function(dao) {
 
     function(id) {
         ns <- NS(id)
-        widgets <- map(attributes, function(a) {
-            textInput(ns(a), a)
+        widgets <- map(names(attributes), function(name) {
+            a <- attributes[[name]]
+            if (a$type == 'factor') {
+                selectInput(ns(name), name, choices = a$levels)
+            } else if (a$type == 'numeric') {
+                numericInput(ns(name), name, value = NA)
+            } else if (a$type == 'logical') {
+                checkboxInput(ns(name), name)
+            } else {
+                textInput(ns(name), name, value = NA)
+            }
         })
         do.call(formUI, c(list(id), widgets))
     }
