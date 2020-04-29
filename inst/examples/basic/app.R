6b39320e9ab41fe4a9658cc98fcf0e409a3f6a78
Michal Burda <michal.burda@centrum.cz>
2020-04-29 11:57:28 +0200

    Code polishing

--- inst/examples/basic/app.R
+++ inst/examples/basic/app.R
@@ -13,7 +13,7 @@ myFormUI <- function(id) {
         selectInput(ns('Type'), 'Type', choices = levels(CO2$Type)),
         selectInput(ns('Treatment'), 'Treatment', choices = levels(CO2$Treatment)),
         numericInput(ns('conc'), 'Ambient CO2 concentration [ml/L]', value = 100, min = 50, max = 1000),
-        numericInput(ns('uptake'), 'CO2 uptake rates [umol/m2 sec]', value = 0, min = 0, max = 100),
+        numericInput(ns('uptake'), 'CO2 uptake rates [umol/m2 sec]', value = 0, min = 0, max = 100)
     )
 }
 
