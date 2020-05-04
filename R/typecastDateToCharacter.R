6848cb1e3b7252f169079bfcec81961dcf1b4b0e
Michal Burda <michal.burda@centrum.cz>
2020-05-04 13:58:58 +0200

    Updated documentation

--- R/typecastDateToCharacter.R
+++ R/typecastDateToCharacter.R
@@ -1,6 +1,8 @@
 #' Create a typecast object that converts between the output \code{\link{Date}} and internal
 #' \code{character} data type
 #'
+#' @param format the format of the character representation of the date (see \code{\link{strptime}}
+#'   for more information)
 #' @return An instance of the \code{'typecast'} class
 #' @seealso \code{\link{typecast}}, \code{\link{typecastDateToNumeric}}
 #' @export
